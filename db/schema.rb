c967903397c8f152b2518ecc2320788f7ecd8a87
Sean Robenalt <seanaltonrobenalt@gmail.com>
2018-02-02 22:40:13 -0500

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,15 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20180203031257) do
 
-  create_table "apps", force: :cascade do |t|
-    t.string "name"
-    t.string "url"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_apps_on_user_id"
-  end
-
   create_table "payments", force: :cascade do |t|
     t.string "cus_id"
     t.integer "amount"
